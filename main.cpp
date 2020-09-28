9eacffce06830a9749c4aa53316e512254ed9220
Елисей Ли <elisey.li.2002@mail.ru>
2020-09-28 19:14:48 +1000

    included files in my prog

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    int a = 5;
-    cout << "Enter\n";
-    cout << a*12 << endl;
-
+    ifstream fin ("input.txt");
+    ofstream fout ("output.txt");
+    
+    fin.close();
+    fout.close();
 
     return 0;
 }
